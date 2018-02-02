ed8a849b77607abd8e150f4ef6ebb7ecd5e8fc66
ismaelalvesgit <cearaismael1997@gmail.com>
2018-02-02 09:49:58 -0300

    exemplos

---
+++ es6/arrow_function.js
@@ -0,0 +1,35 @@
+/*var dobroDoValor = function(numero){
+
+    return numero * 2
+}
+
+console.log(dobroDoValor(7))
+*/
+
+/*var dobroDoValor = numero => {
+
+    return numero * 2
+}
+
+console.log(dobroDoValor(7))
+*/
+
+/*let dobroDoValor = function (numero, numero2) {
+
+     return (numero * numero2)
+}
+
+console.log(dobroDoValor(6, 7))
+*/
+
+let dobroDoValor = function (numero, numero2) {
+
+    let resultado = (numero * numero2)
+
+    return resultado
+}
+
+console.log(dobroDoValor(6, 7))
+/* 
+    exemplos de basicos de como usar arrow_function
+*/
---
+++ es6/variavel.js
@@ -0,0 +1,37 @@
+var nome = 'ismael'
+
+if(true){
+
+    let nome = 'andressa'
+    
+    console.log(nome)
+}
+
+console.log(nome)
+
+/*
+let nome = 'ismael'
+
+if(true){
+
+    var nome = 'andressa'
+}
+
+console.log(nome)
+
+OBS: como não fazer o hosten
+*/
+
+
+/*
+exemplos basico de escorpo global, escopo de função, escopo de bloco
+
+escorpo global = escorpo do script
+
+escorpo função = sepre associado a função abre e fecha {} a companhado de funtion
+
+escorpo bloco = blocos de codico que geralmente estão associados em uma função
+ 
+hosten = a varivel sofre uma elevação de escorpo
+
+*/
