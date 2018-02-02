b138751eafe57309c3de8e60d6b2e74651ec0966
ismaelalvesgit <cearaismael1997@gmail.com>
2018-02-02 11:03:55 -0300

    exemplos

---
+++ typescript/app.ts
@@ -0,0 +1,27 @@
+/*
+let ola = (nome:string) => {
+
+    console.log("ola " + nome)
+}
+
+ola('ismael')
+*/
+
+/*
+let mensagem:string = "Seja bem vendo !!"
+let temporadasFriends: number = 10
+let estudandoAngular: boolean = true
+
+let listaDeFrutas: Array<string> = ['Uva', 'Banana', 'Abacaxi']
+let listaDeFrutas2: string[] = ['Uva', 'Banana', 'Abacaxi']
+
+let notasDasProvas: Array<number> = [7.5, 8, 9]
+let notasDasProvas2: number[] = [7.5, 8, 9]
+*/
+
+/*
+    exemplos basico de tipagem no typescript
+
+    para rodar a transformação de ts para es5 utlizar
+    comando tsc -w no promt no diretorio que esta sendo utlizado
+*/
